ff87615010a3946f2ca00b15b235923d41de9b82
Liam Berezowsky <liam.berezow@gmail.com>
2024-02-06 19:51:50 -0500

    add initial loop

--- telemetry.py
+++ telemetry.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python3
 
 
+class TemperatureSensor:
+
+    def read(_self):
+        return 10
+
+
 if __name__ == '__main__':
-    print('hello')
+    print('telemetry active')
+
+    temperatureSensor = TemperatureSensor()
+    while True:
+        temperature =temperatureSensor.read() 
+        print(temperature)
