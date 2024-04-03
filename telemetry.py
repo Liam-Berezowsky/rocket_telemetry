1f03fb39edfcbdb7001111dd3ba1b819a72fa870
Liam Berezowsky <liam.berezow@gmail.com>
2024-04-02 20:00:56 -0400

    working environment sensor

--- telemetry.py
+++ telemetry.py
@@ -3,7 +3,8 @@
 import random
 from time import sleep 
 import config
-
+import smbus2
+import bme280
 
 class TemperatureSensor:
 
@@ -13,9 +14,10 @@ class TemperatureSensor:
 
 if __name__ == '__main__':
     print('telemetry active')
-
-    temperatureSensor = TemperatureSensor()
+    address = 0x76
+    bus = smbus2.SMBus(1)
+    calibration_params = bme280.load_calibration_params(bus, address)
     while True:
-        temperature =temperatureSensor.read() 
-        print(temperature)
+        data = bme280.sample(bus, address, calibration_params)
+        print("{:.2f},{:.2f},{:.2f}".format(data.temperature, data.pressure, data.humidity))
         sleep(config.time_delay_s)
