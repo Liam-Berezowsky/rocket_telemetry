333d5b8300a2c4ae18538e466103d15e27aa87e1
Liam Berezowsky <liam.berezow@gmail.com>
2024-04-05 20:57:22 -0400

    time added

--- telemetry.py
+++ telemetry.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import random
-from time import sleep 
+from time import sleep, gmtime, strftime, time
 import config
 import smbus2
 import bme280
@@ -17,7 +17,13 @@ if __name__ == '__main__':
     address = 0x76
     bus = smbus2.SMBus(1)
     calibration_params = bme280.load_calibration_params(bus, address)
+    starttime = time()
+    print("time,MET,temperature,pressure,humidity")
     while True:
         data = bme280.sample(bus, address, calibration_params)
-        print("{:.2f},{:.2f},{:.2f}".format(data.temperature, data.pressure, data.humidity))
+        now = time()
+        met = now - starttime
+        now_str = strftime("%d/%m/%Y %H:%M:%S", gmtime(now))
+        rem = int((now - int(now)) * 10000)
+        print("{}.{:.0f},{:.4f},{:.2f},{:.2f},{:.2f}".format(now_str, rem, met, data.temperature, data.pressure, data.humidity))
         sleep(config.time_delay_s)
