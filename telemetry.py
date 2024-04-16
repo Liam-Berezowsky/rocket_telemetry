a6e7492622ddc9ff34ff1f3c6ecb85d4ee264a41
Liam Berezowsky <liam.berezow@gmail.com>
2024-04-16 19:39:46 -0400

    refactor timer

--- telemetry.py
+++ telemetry.py
@@ -6,6 +6,7 @@ import config
 import smbus2
 import bme280
 
+
 class EnvironmentSensor:
 
     def __init__(self, bus, address):
@@ -17,17 +18,28 @@ class EnvironmentSensor:
         return bme280.sample(self.bus, self.address, self.calibration_params)
 
 
+class Timer:
+   
+    def __init__(self):
+        self.starttime = time()
+
+    def str(self):
+        now = time()
+        now_str = strftime("%d/%m/%Y %H:%M:%S", gmtime(now))
+        rem = int((now - int(now)) * 10000)
+        return "{}.{:.0f}".format(now_str, rem)
+
+    def met(self):
+        return time() - self.starttime
+
+
 if __name__ == '__main__':
     print('telemetry active')
     bus = smbus2.SMBus(1)
     esensor = EnvironmentSensor(bus, 0x76)
-    starttime = time()
+    timer = Timer()
     print("time,MET,temperature,pressure,humidity")
     while True:
         data = esensor.read()
-        now = time()
-        met = now - starttime
-        now_str = strftime("%d/%m/%Y %H:%M:%S", gmtime(now))
-        rem = int((now - int(now)) * 10000)
-        print("{}.{:.0f},{:.4f},{:.2f},{:.2f},{:.2f}".format(now_str, rem, met, data.temperature, data.pressure, data.humidity))
+        print("{},{:.4f},{:.2f},{:.2f},{:.2f}".format(timer.str(), timer.met(), data.temperature, data.pressure, data.humidity))
         sleep(config.time_delay_s)
