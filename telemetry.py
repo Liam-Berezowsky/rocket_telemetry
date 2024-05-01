5be4eb09b0521e289aa1037767942d57aa5a8b2c
Liam Berezowsky <liam.berezow@gmail.com>
2024-04-30 20:12:25 -0400

    add positional sensor data output

--- telemetry.py
+++ telemetry.py
@@ -18,6 +18,73 @@ class EnvironmentSensor:
         return bme280.sample(self.bus, self.address, self.calibration_params)
 
 
+class PositionalData:
+
+    def __init__(self, Ax, Ay, Az, Gx, Gy, Gz):
+        self.Ax = Ax
+        self.Ay = Ay
+        self.Az = Az
+        self.Gx = Gx
+        self.Gy = Gy
+        self.Gz = Gz
+
+
+class PositionalSensor:
+
+    def __init__(self, bus, Device_Address):
+        self.PWR_MGMT_1   = 0x6B
+        self.SMPLRT_DIV   = 0x19
+        self.CONFIG       = 0x1A
+        self.GYRO_CONFIG  = 0x1B
+        self.INT_ENABLE   = 0x38
+        self.ACCEL_XOUT_H = 0x3B
+        self.ACCEL_YOUT_H = 0x3D
+        self.ACCEL_ZOUT_H = 0x3F
+        self.GYRO_XOUT_H  = 0x43
+        self.GYRO_YOUT_H  = 0x45
+        self.GYRO_ZOUT_H  = 0x47 
+        self.bus = bus
+        self.Device_Address = Device_Address
+
+        #write to sample rate register
+        bus.write_byte_data(Device_Address, self.SMPLRT_DIV, 7)
+        
+        #Write to power management register
+        bus.write_byte_data(Device_Address, self.PWR_MGMT_1, 1)
+        
+        #Write to Configuration register
+        bus.write_byte_data(Device_Address, self.CONFIG, 0)
+            
+        #Write to Gyro configuration register
+        bus.write_byte_data(Device_Address, self.GYRO_CONFIG, 24)
+    
+        #Write to interrupt enable register
+        bus.write_byte_data(Device_Address, self.INT_ENABLE, 1)
+
+    def read_raw_data(self, addr):
+        #Accelero and Gyro value are 16-bit
+        high = self.bus.read_byte_data(self.Device_Address, addr)
+        low = self.bus.read_byte_data(self.Device_Address, addr+1)
+        
+        #concatenate higher and lower value
+        value = ((high << 8) | low)
+            
+        #to get signed value from mpu6050
+        if(value > 32768):
+            value = value - 65536
+        return value
+
+    def read(self):
+        return PositionalData(
+            self.read_raw_data(self.ACCEL_XOUT_H)/16384.0,
+            self.read_raw_data(self.ACCEL_YOUT_H)/16384.0,
+            self.read_raw_data(self.ACCEL_ZOUT_H)/16384.0,
+            self.read_raw_data(self.GYRO_XOUT_H)/131.0,
+            self.read_raw_data(self.GYRO_YOUT_H)/131.0,
+            self.read_raw_data(self.GYRO_ZOUT_H)/131.0
+        )
+
+
 class Timer:
    
     def __init__(self):
@@ -37,9 +104,23 @@ if __name__ == '__main__':
     print('telemetry active')
     bus = smbus2.SMBus(1)
     esensor = EnvironmentSensor(bus, 0x76)
+    psensor = PositionalSensor(bus, 0x68)
     timer = Timer()
-    print("time,MET,temperature,pressure,humidity")
+    print("time,MET,temperature,pressure,humidity,acc_x,acc_y,acc_z,gyr_x,gyr_y,gyr_z")
     while True:
-        data = esensor.read()
-        print("{},{:.4f},{:.2f},{:.2f},{:.2f}".format(timer.str(), timer.met(), data.temperature, data.pressure, data.humidity))
+        edata = esensor.read()
+        pdata = psensor.read()
+        print("{},{:.4f},{:.2f},{:.2f},{:.2f},{:.2f},{:.2f},{:.2f},{:.2f},{:.2f},{:.2f}".format(
+            timer.str(),
+            timer.met(),
+            edata.temperature, 
+            edata.pressure, 
+            edata.humidity,
+            pdata.Ax,
+            pdata.Ay,
+            pdata.Az,
+            pdata.Gx,
+            pdata.Gy,
+            pdata.Gz
+            ))
         sleep(config.time_delay_s)
